4a0e77074df687658ca32a348c948cf206139729
sunyaowei <sunyaowei@hollytop.cn>
2022-12-08 15:06:11 +0800

    commit msg: 增加安全校验模块

---
+++ sw-sprintboot-security/src/main/java/com/sw/xyz/springframework/security/config/SecurityProperties.java
@@ -0,0 +1,13 @@
+package com.sw.xyz.springframework.security.config;
+
+/**
+ * 名称: SecurityProperties
+ * 功能: <功能详细描述>
+ * 方法: <方法简述-方法描述>
+ * 版本: 1.0
+ * 作者: sunyw
+ * 说明: 说明描述
+ * 时间: 2022/12/8 0008 15:05
+ */
+public class SecurityProperties {
+}
---
+++ sw-sprintboot-security/src/main/java/com/sw/xyz/springframework/security/filter/SecurityAutoConfig.java
@@ -0,0 +1,13 @@
+package com.sw.xyz.springframework.security.filter;
+
+/**
+ * 名称: SecurityAutoConfig
+ * 功能: <功能详细描述>
+ * 方法: <方法简述-方法描述>
+ * 版本: 1.0
+ * 作者: sunyw
+ * 说明: 说明描述
+ * 时间: 2022/12/8 0008 15:06
+ */
+public class SecurityAutoConfig {
+}
